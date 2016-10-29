3a4b2ebd8eafdf765f900fb7e9bee0f202f89222
rahulgs12 <gsr1998@gmail.com>
2016-10-29 15:39:48 -0400

    Detecting end of high five

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,30 @@
 var socket = io();
 var gameId = document.getElementById("gameId").innerHTML;
 var socketId;
-
+var highFiving = false;
 socket.emit("init", gameId);
 socket.on("init", function(id) {
   socketId = id;
 });
+
+var threshhold = 15;
+
 var max = 0;
 window.ondevicemotion = function(event){
-  var acce = event.accelerationIncludingGravity.x + event.accelerationIncludingGravity.y + event.accelerationIncludingGravity.z;
+  var acce = Math.abs(event.accelerationIncludingGravity.x) + Math.abs(event.accelerationIncludingGravity.y)+ Math.abs(event.accelerationIncludingGravity.z);
 
-if (acce>max){
+if (acce>max && !highFiving){
   max=acce;
   document.getElementById("acc").innerHTML = max;
 }
+
+if (max>threshhold){
+  highFiving = true;
+
+}
+ if (highFiving && acce<threshhold){
+   document.getElementById("status").innerHTML = "High FIVE! Max value was " + max;
+ }
+
 //document.getElementById("acc").innerHTML = document.getElementById("acc").innerHTML +"<br>" + x+ " " + y + " " + z;
 }
