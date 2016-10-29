73f38c3e5193370d3413a2878d4ec57e3658b0f6
williamyeny <williamyeny@gmail.com>
2016-10-29 13:27:34 -0400

    potential req use

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 
+// var bodyParser = require('body-parser');
+// app.use( bodyParser.json() );       // to support JSON-encoded bodies
+// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//   extended: true
+// }));
+
 var port = process.env.PORT || 8080;
 
 app.use(express.static("public"));
