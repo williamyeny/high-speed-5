f3dffc5f51d7bf8e93b99c5ff2252fc8d3434a98
williamyeny <williamyeny@gmail.com>
2016-10-29 13:28:02 -0400

    adding creating/joining games

--- index.js
+++ index.js
@@ -9,12 +9,36 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+var games = {};
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
 
-  res.render('index', {});
+  res.render('index');
+});
+
+app.get("/create", function(req, res) {
+  var randomId = 0;
+  do {
+   randomId  = Math.floor(Math.random()*900000) + 100000;
+  } while (randomId in games);
+
+  games[randomId] = {};
+  res.redirect("/game/randomId");
+});
+
+app.get("/game/:gameId", function(req, res) {
+  if (Object.keys(games[gameId]).length == 2) {
+    res.render('oops', {message: "This game is full!"});
+  }
+
+  if (!gameId in games) {
+    res.render("oops", {message: "Game not found!"});
+  }
+
+  res.render('game', {gameId: gameId});
 });
 
 var server = app.listen(port, function() {
