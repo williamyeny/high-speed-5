13bf4b3e806df3be91a4657253b060af432759ce
williamyeny <williamyeny@gmail.com>
2016-10-29 15:51:47 -0400

    more debugging:

--- public/js/script.js
+++ public/js/script.js
@@ -9,22 +9,26 @@ socket.on("init", function(id) {
 
 var threshhold = 40;
 
-var max = 0;
+var highFiveMax = 0;
+var overAllMax = 0;
 window.ondevicemotion = function(event){
   var acce = Math.abs(event.accelerationIncludingGravity.x) + Math.abs(event.accelerationIncludingGravity.y)+ Math.abs(event.accelerationIncludingGravity.z);
-  document.getElementById("acc").innerHTML = max;
+  document.getElementById("acc").innerHTML = overAllMax;
 
-if (acce>max && !highFiving){
-  max=acce;
-
+if (acce>highFiveMax && !highFiving){
+  highFiveMax=acce;
 }
 
-if (max>threshhold){
+if (acce>overAllMax) {
+  overAllMax = acce;
+}
+
+if (highFiveMax>threshhold){
   highFiving = true;
 
 }
  if (highFiving && acce<threshhold){
-   document.getElementById("status").innerHTML = "High FIVE! Max value was " + max;
+   document.getElementById("status").innerHTML = "High FIVE! Max value was " + highFiveMax;
  }
 
 //document.getElementById("acc").innerHTML = document.getElementById("acc").innerHTML +"<br>" + x+ " " + y + " " + z;
