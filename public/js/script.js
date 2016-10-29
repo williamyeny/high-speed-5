48dced4281af8f9db238416c681cc5513e384cd8
williamyeny <williamyeny@gmail.com>
2016-10-29 16:02:38 -0400

    better max code

--- public/js/script.js
+++ public/js/script.js
@@ -7,29 +7,24 @@ socket.on("init", function(id) {
   socketId = id;
 });
 
-var threshhold = 40;
+var threshhold = 50;
+var highFived = false;
 
-var highFiveMax = 0;
-var overAllMax = 0;
+var max = 0;
 window.ondevicemotion = function(event){
   var acce = Math.abs(event.accelerationIncludingGravity.x) + Math.abs(event.accelerationIncludingGravity.y)+ Math.abs(event.accelerationIncludingGravity.z);
-  document.getElementById("acc").innerHTML = overAllMax;
 
-if (acce>highFiveMax && !highFiving){
-  highFiveMax=acce;
+if (acce>max){
+  max=acce;
+  document.getElementById("acc").innerHTML = max;
 }
 
-if (acce>overAllMax) {
-  overAllMax = acce;
+if (max>threshhold && highFived){
+  setTimeout(function() {
+    highFived = true;
+    document.getElementById("status").innerHTML = "High FIVE! Max value was " + max;
+  }, 1500);
 }
-
-if (highFiveMax>threshhold){
-  highFiving = true;
-
-}
- if (highFiving && acce<threshhold){
-   document.getElementById("status").innerHTML = "High FIVE! Max value was " + highFiveMax;
- }
 
 //document.getElementById("acc").innerHTML = document.getElementById("acc").innerHTML +"<br>" + x+ " " + y + " " + z;
 }
