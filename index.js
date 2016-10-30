92b9fc6388cf3fd2ed46e08c016362066d6a25fb
williamyeny <williamyeny@gmail.com>
2016-10-30 08:36:16 -0400

    more favicon

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var express = require("express");
 var app = express();
+var favicon = require('serve-favicon');
 
-// var bodyParser = require('body-parser');
-// app.use( bodyParser.json() );       // to support JSON-encoded bodies
-// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//   extended: true
-// }));
 var port = process.env.PORT || 8080;
 
 var games = {};
 
 app.use(express.static("public"));
+app.use(favicon(__dirname + "/favicon.ico"))
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
